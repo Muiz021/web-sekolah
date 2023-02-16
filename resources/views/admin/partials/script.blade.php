93b58524ead0693de3aad6c1391aa596fca7647b
Ikhsandi-Ramadani <ikhsandiramadani1337@gmail.com>
2023-02-16 20:34:09 +0800

    Base App

---
+++ resources/views/admin/partials/script.blade.php
@@ -0,0 +1,18 @@
+ <!-- Core JS -->
+ <!-- build:js assets/vendor/js/core.js -->
+ <script src="{{ asset('back/vendor/libs/jquery/jquery.js') }}"></script>
+ <script src="{{ asset('back/vendor/libs/popper/popper.js') }}"></script>
+ <script src="{{ asset('back/vendor/js/bootstrap.js') }}"></script>
+ <script src="{{ asset('back/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+ <script src="{{ asset('back/vendor/libs/hammer/hammer.js') }}"></script>
+
+ <script src="{{ asset('back/vendor/js/menu.js') }}"></script>
+ <!-- endbuild -->
+
+ <!-- Vendors JS -->
+
+ <!-- Main JS -->
+ <script src="{{ asset('back/js/main.js') }}"></script>
+
+ <!-- Page JS -->
---
+++ resources/views/admin/partials/style.blade.php
@@ -0,0 +1,29 @@
+<!-- Favicon -->
+<link rel="icon" type="image/x-icon" href="{{ asset('back/img/favicon/favicon.ico') }}" />
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet" />
+
+<!-- Icons. Uncomment required icon fonts -->
+<link rel="stylesheet" href="{{ asset('back/vendor/fonts/boxicons.css') }}" />
+
+<link rel="stylesheet" href="{{ asset('back/vendor/fonts/fontawesome.css') }}" />
+<link rel="stylesheet" href="{{ asset('back/vendor/fonts/flag-icons.css') }}" />
+
+<!-- Core CSS -->
+<link rel="stylesheet" href="{{ asset('back/vendor/css/rtl/core.css') }}" />
+<link rel="stylesheet" href="{{ asset('back/vendor/css/rtl/theme-default.css') }}" />
+<link rel="stylesheet" href="{{ asset('back/css/demo.css') }}" />
+
+<!-- Vendors CSS -->
+<link rel="stylesheet" href="{{ asset('back/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+
+<!-- Page CSS -->
+
+<!-- Helpers -->
+<script src="{{ asset('back/vendor/js/helpers.js') }}"></script>
+<script src="{{ asset('back/js/config.js') }}"></script>
