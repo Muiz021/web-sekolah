aa98687f210efe0bb7a5df8739d67595b11a5855
Muiz Muharram <72422975+Muiz021@users.noreply.github.com>
2023-11-11 20:12:40 +0800

    first commit

--- resources/views/admin/pages/dashboard/index.blade.php
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -74,15 +74,15 @@
                 <div class="card bg-secondary text-white mb-3">
                     <div class="card-header">Jumlah Data Berita</div>
                     <div class="card-body">
-                       <div class="d-flex justify-content-between">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24"
-                        style="fill: #fff;transform: ;msFilter:;">
-                        <path
-                            d="M20 17a1 1 0 0 1-2 0V5a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v13a2 2 0 0 0 2 2h15c1.654 0 3-1.346 3-3V7h-2v10zM12 7h3v2h-3V7zm0 4h3v2h-3v-2zM5 7h5v6H5V7zm0 10v-2h10v2H5z">
-                        </path>
-                    </svg>
-                    <h3 class="text-white" style="font-size: 72px">{{ BeritaSekolah::count() }}</h3>
-                       </div>
+                        <div class="d-flex justify-content-between">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24"
+                                style="fill: #fff;transform: ;msFilter:;">
+                                <path
+                                    d="M20 17a1 1 0 0 1-2 0V5a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v13a2 2 0 0 0 2 2h15c1.654 0 3-1.346 3-3V7h-2v10zM12 7h3v2h-3V7zm0 4h3v2h-3v-2zM5 7h5v6H5V7zm0 10v-2h10v2H5z">
+                                </path>
+                            </svg>
+                            <h3 class="text-white" style="font-size: 72px">{{ BeritaSekolah::count() }}</h3>
+                        </div>
                     </div>
                 </div>
             </div>
